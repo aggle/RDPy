e080005ab7df1cd3b9e73a47c76f280029bbac43
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2019-08-30 01:03:07 -0400

    Added more tests for reference ranking functions
    Currently, they all fail. Not sure if the problem is with the functions or with
    the tests themselves

--- testing/test_RDI.py
+++ testing/test_RDI.py
@@ -3,6 +3,11 @@ from .. import RDI
 import pytest
 from numpy import testing as nptest
 from pathlib import Path
+import itertools
+
+# change this line to change the HPF widths you want to test
+hpf_index = [0, 5, 10, 15, 20, 25, 30, 35, 40]
+hpf_index = hpf_index[::4]
 
 
 @pytest.fixture()
@@ -26,6 +31,17 @@ def generate_test_data():
                                [4, 3, 2, 1]])
     return targ, references
 
+@pytest.fixture
+def real_data():
+    """
+    Loads the beta Pic, HR 8799, and reference data for testing
+    Dataframe columns:
+    """
+    #initial_data_path = Path(__file__).resolve().parent / '..' / '..' / 'BetaPic' / 'staticdata'
+    #data_path = initial_data_path / 'init_data_hpf.pkl'
+    #print('oops', data_path)
+    data_df = RDI.pd.read_pickle('/Users/jaguilar/Projects/BetaPic/staticdata/init_data_hpf.pkl')
+    return(data_df)
 
 def test_mean_squared_error(generate_test_data):
     """
@@ -46,7 +62,43 @@ def test_pearson_correlation(generate_test_data):
     targ, references = generate_test_data
     # Exercise
     pcc = RDI.calc_refcube_pcc(targ, references)
-    truth = RDI.np.array([4/3, 4/3, RDI.np.nan, -4/3])
+    truth = RDI.np.array([1., 1., RDI.np.nan, -1.]) # RDI.np.array([4/3, 4/3, RDI.np.nan, -4/3])
     # Validate
     nptest.assert_array_almost_equal(pcc, truth, 13)
+    
     # Cleanup - none
+
+@pytest.mark.parametrize('hpf, test_func',
+                         list(itertools.product(hpf_index,
+                                                [RDI.ssim_luminance,
+                                                 RDI.ssim_contrast,
+                                                 RDI.ssim_structural])
+                         )
+)
+def test_ssim_helper_functions(real_data, hpf, test_func):
+    """
+    All values should be between -1 and 1
+    """
+    # cut the number of references used by 4 to save time for
+    result = test_func(real_data.loc[hpf, 'betaPic'],
+                       real_data.loc[hpf, 'refcube'][::4])
+    assert(RDI.np.all((-1<= result) & (result <= 1)))
+
+@pytest.mark.skip("redundant?")
+def test_ssim_luminance(real_data, hpf):
+    """
+    Test the Luminance part of the calculation.
+    All values should be between 0 and 1
+    """
+    # cut the number of references used by 4 to save time for
+    lum = RDI.ssim_luminance(real_data.loc[hpf, 'hr8799'][0],
+                             real_data.loc[hpf, 'refcube'][::4])
+    assert(RDI.np.all((0 <= lum) & (lum <= 1)))
+
+
+
+def test_structural_similarity(generate_test_data):
+    """
+    Value should be only between 0 and 1
+    """
+    pass
