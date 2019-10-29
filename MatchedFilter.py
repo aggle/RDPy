Fixed MF normalization and some utility functions
Now, the MF is not automatically normalized. You must normalize it manually by
calling the appropriate normalization helper function.

changes to other functions were minor fixes

MatchedFilter.py
@@ -14,9 +14,50 @@ def calc_matched_filter_throughput(matched_filter):
       throughput: the throughput correction for the matched filter
          correct signal = (matched_filter_result) / throughput
     """
-    throughput = np.linalg.norm(matched_filter)**2
+    orig_shape = matched_filter.shape
+    nan_coords = np.isnan(matched_filter)
+    matched_filter[nan_coords] = 0
+    throughput = np.linalg.norm(utils.flatten_image_axes(matched_filter), axis=-1)**2
+    matched_filter[nan_coords] = np.nan
     return throughput
 
+def calc_matched_filter_throughput_klip(matched_filter, locations, kl_basis, num_basis, return_stamps=False):
+    """
+    Calculate the KLIP-corrected matched filter throughput
+    Args:
+      matched_filter: the matched filter stamp
+      locations: Npix x 2 array of (row, col) locations you care about
+      kl_basis: the full-image KLIP basis as [N_k, Nrow, Ncol] i.e. not raveled)
+      num_basis: array of the number of bases
+      return_stamp [False]: if True, return the KL-subtracted matched filters for debugging
+    Returns:
+      N_locations array of throughput corrections
+    """
+    # first, get rid of nans in the KL basis
+    kl_basis[np.isnan(kl_basis)] = 0
+    stamp_shape = matched_filter.shape
+    img_shape = kl_basis.shape[-2:]
+    mf_klsub = np.zeros((len(locations), len(num_basis), stamp_shape[0]*stamp_shape[1]),
+                        dtype=np.float)
+    for i, loc in enumerate(locations):
+        if (i+1)%400 == 0:
+            print(f"{i+1} throughputs of f{len(locations)} completed")
+        # pull out the KL basis stamp
+        loc_ravel = np.int(np.ravel_multi_index(loc, img_shape))
+        _, stamp_coords = utils.get_stamp_coordinates(loc, stamp_shape[0], stamp_shape[1], img_shape)
+        klip_stamp = utils.get_stamp_from_cube(kl_basis, stamp_shape, loc_ravel)
+        # do klip subtraction with the stamp
+        klsub_result = RK.klip_subtract_with_basis(matched_filter.ravel(),
+                                                  utils.flatten_image_axes(klip_stamp),
+                                                  num_basis)
+        mf_klsub[i] = klsub_result
+
+    # return KL-subtracted stamp for debugging
+    if return_stamps == True:
+        return mf_klsub
+    throughput = calc_matched_filter_throughput(utils.make_image_from_region(mf_klsub))
+
+    return throughput
 
 def create_matched_filter(template):
     """
@@ -25,7 +66,7 @@ def create_matched_filter(template):
     """
     matched_filter = template - np.nanmean(template)
     throughput = calc_matched_filter_throughput(matched_filter)
-    matched_filter = matched_filter/throughput
+    #matched_filter = matched_filter/throughput
     return matched_filter
 
 
@@ -157,7 +198,6 @@ def apply_matched_filter_fft_to_cube(cube, matched_filter):
     mf_result = np.array(list(map(lambda img: apply_matched_filter_fft(img, matched_filter), cube)))
     mf_result = np.reshape(mf_result, orig_shape)
     return mf_result
-
 
 
 

RDIklip.py
@@ -142,20 +142,16 @@ def klip_subtract_with_basis_slower(img_flat, kl_basis, n_bases=None,
     return np.squeeze(kl_sub.reshape(new_shape))
 
 
+def generate_klip_psf(img_flat, kl_basis, n_bases=None):
+    """
+    Generate the KLIP model of the PSF
+    """
+    pass
+
 def klip_subtract_with_basis(img_flat, kl_basis, n_bases=None):
     """
-    If you already have the KL basis, do the klip subtraction
-    Arguments:
-      img_flat: (Nparam x) Npix flattened image - pixel axis must be last
-      kl_basis: (Nbases x ) Nklip x Npix array of KL basis vectors (possibly more than one basis)
-      n_bases [None]: list of integers for Kmax, return one image per Kmax in n_bases.
-          If None, use full basis
-      double_project: apply KL twice (useful for some FMMF cases)
-    Return:
-      kl_sub: array with same shape as input image, after KL PSF subtraction.
-        The KL basis axis is second-to-last (before image pixels)
+    Generate the KLIP model of the PSF
     """
-
     # make sure n_bases is iterable
     if n_bases is None:
         n_bases = np.array([len(kl_basis)])
@@ -204,3 +200,24 @@ def klip_subtract_with_basis(img_flat, kl_basis, n_bases=None):
     return np.squeeze(kl_sub.reshape(new_shape))
 
 
+"""
+def klip_subtract_with_basis(img_flat, kl_basis, n_bases=None):
+    If you already have the KL basis, do the klip subtraction
+    Arguments:
+      img_flat: (Nparam x) Npix flattened image - pixel axis must be last
+      kl_basis: (Nbases x ) Nklip x Npix array of KL basis vectors (possibly more than one basis)
+      n_bases [None]: list of integers for Kmax, return one image per Kmax in n_bases.
+          If None, use full basis
+      double_project: apply KL twice (useful for some FMMF cases)
+    Return:
+      kl_sub: array with same shape as input image, after KL PSF subtraction.
+        The KL basis axis is second-to-last (before image pixels)
+    klip_psf = generate_klip_psf(img_flat, kl_basis, n_bases)
+
+    img_flat_mean_sub = img_flat - np.nanmean(img_flat, axis=-1, keepdims=True)
+    # tile the target to match the shape of klip_psf
+    imgs_tiled = np.tile(img_flat_mean_sub[..., None, :],
+                         [1 for i in img_flat_mean_sub.shape[:-1]] + [len(klip_psf), 1])
+    klip_sub = imgs_tiled - klip_psf
+    return klip_sub
+"""

testing/test_utils.py
@@ -47,7 +47,7 @@ def test_get_cube_of_stamps():
     rad = 10
     region_mask = utils.make_circular_mask((40, 40), rad, NICMOS_imshape)
     pixel_indices = utils.np.ravel_multi_index(utils.np.where(region_mask), NICMOS_imshape)
-    stamp_cube = utils.convert_image_to_cube_of_stamps(region_mask, stamp_shape, pixel_indices)
+    stamp_cube = utils.get_cube_of_stamps_from_image(region_mask, stamp_shape, pixel_indices)
     # Validate
     assert(stamp_cube.shape == (len(pixel_indices), stamp_shape[0], stamp_shape[1]))
 
@@ -61,4 +61,14 @@ def test_get_cube_of_stamps():
     # are truly in the center
     assert(utils.np.sum(stamp_cube[cent_stamps]) == len(cent_stamps)*stamp_shape[0]*stamp_shape[1])
     #assert(np.nansum(stamp_cube) == 100)
+    # Cleanup - none
+
+def test_orient_alice_image():
+    # setup
+    image = utils.np.ones((100,100))
+    angle = 90
+    # Exercise
+    rot_img = utils.orient_alice_image(image, orientat=angle)
+    # Validate
+    assert(utils.np.shape(rot_img) == utils.np.shape(image))
     # Cleanup - none

utils.py
@@ -13,7 +13,7 @@ from scipy import ndimage
 #def rot_mat(angle):
 #    """
 #    Generate a rotation matrix for a given angle in degrees. 
-#    It is designed to operate on images in python and rotate them
+#    It is designed to operate on image coordinates in python and rotate them
 #    such that north is up and east is to the left (i.e. CCW).
 #    Args:
 #        angle: rotation angle in degrees
@@ -85,13 +85,27 @@ def seppa_2_image(sep, pa, ORIENTAT=0, center=(40,40), pix_scale = 75):
     """
     # convert all angles from degrees to radians
     center = np.array(center)
-    pa = pa*np.pi/180
-    ORIENTAT = ORIENTAT*np.pi/180
+    pa = np.radians(pa)
+    ORIENTAT = np.radians(ORIENTAT)
     # rotation and ORIENTAT are defined CCW so need a -1 here
     tot_ang = -1*(pa-ORIENTAT)
     row = sep*np.cos(tot_ang)/pix_scale + center[0]
     col = sep*np.sin(tot_ang)/pix_scale + center[1]
     return (row, col)
+
+
+def orient_alice_image(image, orientat):
+    """
+    Take an image and orient it so north is up and east is left
+    See also: NICMOS.orient_nup_eleft(image, orientat)
+    Args:
+      image: 2-D image
+      orientat: the 'orientat' ALICE header value corresponding to the image
+    Returns:
+      oriented_image: 2-D image with north up, east left. Shape depends on the angle
+    """
+    rot_img = ndimage.rotate(image, -orientat, axes=[-2,-1])
+    return rot_img
 
 
 ##########
@@ -101,16 +115,17 @@ def seppa_2_image(sep, pa, ORIENTAT=0, center=(40,40), pix_scale = 75):
 def get_stamp_coordinates(center, drow, dcol, imshape, nanpad=False):
     """
     get pixel coordinates for a stamp with width dcol, height drow, and center `center` embedded
-    in an image of dimensions imshape
+    in an image of dimensions imshape.
+    don't use this, use get_stamp_from_image or get_stamp_from_cube
     Arguments:
         center: (row, col) center of the stamp
         drow: height of stamp
         dcol: width of stamp
         imshape: total size of image the stamp is a part of
     Returns:
-        img_coords: the stamp indices for the full image array (i.e. stamp = img[img_coords])
+        img_coords: the stamp indices for the full image array (i.e. stamp = img[img_coords[0], img_coords[1]xo])
         stamp_coords: the stamp indices for selecting the part of the stamp
-                      that goes in the image (i.e. stamp[stamp_coords]).
+                      that goes in the image (i.e. stamp[stamp_coords[0], stamp_coords[1]]).
                       this is relevant for stamps on the edge of the images -
                       e.g. if you want to matched filter the image edge, you should
                       plug these coordinates into your matched filter stamp to
@@ -162,15 +177,14 @@ def get_stamp_from_image(image, stamp_shape, coord):
       coord: the flattened coordinate of the pixel of interest
     Returns:
       stamp_cube: a 3-D array of the stamps, in order of the pixel coordinates
-    Args:
     """
-    try:
-        # if the coordinate isn't flat, return None
-        # this is not a smart way to fail
-        assert(isinstance(coord, np.int) is False)
-    except:
-        print("coord ({0}) in get_stamp_from_image needs to be 1-D flattened coordinate".format(coord))
-        return None
+    #try:
+    #    # if the coordinate isn't an integer, return None
+    #    # this is not a smart way to fail
+    #    assert(isinstance(coord, np.int) == True)
+    #except:
+    #    print("coord ({0}) being automatically converted to int") #in get_stamp_from_image needs to be 1-D flattened coordinate".format(coord))
+    #    coord = np.int(coord)
     row, col= np.unravel_index(coord, image.shape)
     img_coord, stamp_coord = get_stamp_coordinates((row, col),
                                                    *stamp_shape,
@@ -186,6 +200,22 @@ def get_stamp_from_image(image, stamp_shape, coord):
         stamp = tmp
     return stamp
 
+def get_stamp_from_cube(cube, stamp_shape, coord):
+    """
+    Pull a stamp out from an image with a given shape centered at
+    a given pixel.
+    Args:
+      cube: 3-D image
+      stamp_shape: the shape of the stamps
+      coord: the flattened coordinate of the pixel of interest
+    Returns:
+      stamp_cube: a 3-D array of the stamps, in order of the pixel coordinates
+    """
+    cube_shape = cube.shape
+    flat_cube = flatten_leading_axes(cube, -2)
+    stamps = [get_stamp_from_image(img, stamp_shape, coord) for img in flat_cube]
+    stamps = np.array(stamps).reshape(list(cube_shape)[:-2] + list(stamp_shape))
+    return stamps
 
 def get_cube_of_stamps_from_image(image, stamp_shape, pixel_coordinates=None):
     """